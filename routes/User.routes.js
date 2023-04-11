32600d4784680516c8458d1b5196126faf292487
aniketpandey2912 <aniketpandey2912@gmail.com>
2023-04-11 13:55:18 +0530

    corrected mssg

--- routes/User.routes.js
+++ routes/User.routes.js
@@ -15,7 +15,8 @@ userRouter.post("/signup", async (req, res) => {
     let user = await UserModel.find({ email });
 
     if (user.length > 0) {
-      res.send({ mssg: "User already exists, login to continue", err: false });
+      console.log(user);
+      res.send({ mssg: "User already exists, login to continue", err: true });
     } else {
       bcrypt.hash(password, 5, async (err, hash) => {
         if (err) {
